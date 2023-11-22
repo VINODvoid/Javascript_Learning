bfc186666bd109b7889129535de53ba6f5f5cbb8
VINODvoid <iamlearner.v@gmail.com>
2023-11-22 13:35:37 +0530

    "01_Variables"

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1267432
+let accountEmail = "random!@gmail.com"
+var accountPassword = "random"
+let accountCity ="Bangalore"
+let accountState;
+// accountId = 12233445566778 //! Not allowed to change as it is already in use
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
